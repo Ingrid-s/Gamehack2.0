7650b91b5045c6bf372eeb7da09b7143cd78b7d1
ingridsag <starlling@hotmail.com>
2018-02-08 17:36:51 -0600

    subiendo otro repo

---
+++ js/data.js
@@ -0,0 +1,93 @@
+var games =[
+
+{
+ "Title" : "Dragon Ball FighterZ",
+ "Console" : ["Nintendo Switch", "PS4", "Xbox One"],
+  "Ranking" : 4.3,
+ "photo" : "../assets/images/dragonball.jpg"
+},
+
+{
+  "Title" : "Pokémon Switch",
+  "Console" : "Nintendo Switch",
+   "Ranking" : 4.0,
+  "photo" : "../assets/images/pokemonns.jpg"
+},
+
+{
+  "Title" : "Jurassic World Evolution",
+  "Console" : "PS4",
+   "Ranking" : 4.1,
+  "photo" : "../assets/images/jwevolution.jpg"
+},
+
+{
+  "Title" : "Yoshi",
+  "Console" : "Nintendo Switch",
+   "Ranking" : 4.2,
+  "photo" : "../assets/images/yoshi.jpg"
+},
+
+{
+  "Title" : "Ni No Kuni II",
+  "Console" : "PS4",
+   "Ranking" : 4.6,
+  "photo" : "../assets/images/nino.jpg"
+},
+
+{
+  "Title" : "Spiderman",
+  "Console" : "PS4",
+   "Ranking" : 4.4,
+  "photo" : "../assets/images/Spiderman.png"
+ },
+
+{
+  "Title" : "Megaman X",
+  "Console" :  ["Nintendo Switch", "PS4", "Xbox One"],
+   "Ranking" : 4.8,
+  "photo" : "../assets/images/mm11.jpg"
+},
+
+{
+  "Title" : "Final Fantasy XV",
+  "Console" : "Xbox One",
+   "Ranking" : 5.0,
+  "photo" : "../assets/images/finalfantasy.jpg"
+},
+{
+  "Title" : "Gran Turismo Sport",
+  "Console" : "PS4",
+   "Ranking" : 4.1,
+  "photo" : "../assets/images/gtsport.jpg"
+},
+{
+  "Title" : "Mario Kart 8 Deluxe",
+  "Console" : "Nintendo Switch",
+   "Ranking" : 4.0,
+  "photo" : "../assets/images/mariokart.png"
+}
+];
+
+var users =[
+
+{
+ "Name" : "Mario Bros77",
+ "Console" : "Nintendo Switch"
+},
+
+{
+ "Name" : "Bella Gamer",
+ "Console" : "PS4"
+},
+
+{
+ "Name" : "Alan Turing",
+ "Console" : "Xbox One"
+},
+{
+ "Name" : "Batman",
+ "Console" : "PS4"
+},
+
+];
